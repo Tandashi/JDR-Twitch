f026fba2f922702b3c167b0d5f72c5b8b6583c2e
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-07-27 00:02:37 +0200

    feat: Add Tailwindcss Support

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,11 @@ import copy from 'rollup-plugin-copy'
 import replace from '@rollup/plugin-replace';
 import typescript from 'rollup-plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import scss from 'rollup-plugin-scss'
 import * as nodeResolve from '@rollup/plugin-node-resolve';
+
+import postcss from 'rollup-plugin-postcss'
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 
 export default {
   input: ['src/panel.tsx'],
@@ -28,9 +31,11 @@ export default {
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    scss({
-      indentedSyntax: true,
-      output: 'dist/bundle.css'
+    postcss({
+      plugins: [
+        tailwindcss,
+        autoprefixer
+      ]
     }),
     typescript(),
     copy({
--- src/pages/panel.tsx
+++ src/pages/panel.tsx
@@ -4,6 +4,59 @@ import '@styles/panel.sass';
 
 export default class Panel extends React.Component {
   public render(): JSX.Element {
-    return <div className={'panel'}>Hello World!!! React :D</div>;
+    return (
+      <div className={'panel'}>
+        <div
+          className={
+            'origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
+          }
+          role={'menu'}
+          aria-orientation={'vertical'}
+          aria-labelledby={'menu-button'}
+          tabIndex={-1}
+        >
+          <div className={'py-1'} role={'none'}>
+            <a
+              href={'#'}
+              className={'text-gray-700 block px-4 py-2 text-sm'}
+              role={'menuitem'}
+              tabIndex={-1}
+              id={'menu-item-0'}
+            >
+              Account settings
+            </a>
+            <a
+              href={'#'}
+              className={'text-gray-700 block px-4 py-2 text-sm'}
+              role={'menuitem'}
+              tabIndex={-1}
+              id={'menu-item-1'}
+            >
+              Support
+            </a>
+            <a
+              href={'#'}
+              className={'text-gray-700 block px-4 py-2 text-sm'}
+              role={'menuitem'}
+              tabIndex={-1}
+              id={'menu-item-2'}
+            >
+              License
+            </a>
+            <form method={'POST'} action={'#'} role={'none'}>
+              <button
+                type={'submit'}
+                className={'text-gray-700 block w-full text-left px-4 py-2 text-sm'}
+                role={'menuitem'}
+                tabIndex={-1}
+                id={'menu-item-3'}
+              >
+                Sign out
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
