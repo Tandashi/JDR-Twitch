b0d3b701ce80f99ed66e0bece00376b267f23d8d
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-07-28 01:52:02 +0200

    style: Disable Image drag and text selection on search entries

--- src/components/search-entry.tsx
+++ src/components/search-entry.tsx
@@ -10,8 +10,8 @@ interface Props {
 export default class SearchEntry extends React.Component<Props> {
   public render(): JSX.Element {
     return (
-      <div className={'flex flex-row flex-nowrap search-entry search-entry-background rounded'}>
-        <img className={'flex-shrink flex-20 rounded-l min-w-0'} src={this.props.songdata.img_url} />
+      <div className={'select-none flex flex-row flex-nowrap search-entry search-entry-background rounded'}>
+        <img className={'flex-shrink flex-20 rounded-l min-w-0'} draggable={false} src={this.props.songdata.img_url} />
         <div className={'flex-grow-4 flex-shrink-0 flex-80 p-2 min-w-0 self-center'}>
           <p className={'search-entry-text search-entry-title truncate'}>{this.props.songdata.title}</p>
           <p className={'search-entry-text search-entry-artist truncate'}>{this.props.songdata.artist}</p>
