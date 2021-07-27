9525089d34c98dad1fea8ece8cb24ba9b0a198a2
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-07-27 21:33:35 +0200

    feat: Add Searchbar Component

---
+++ src/components/searchbar.tsx
@@ -0,0 +1,49 @@
+import React, { ChangeEvent, ChangeEventHandler } from 'react';
+
+import '@styles/components/searchbar.sass';
+
+interface Props {
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+interface State {
+  value: string;
+}
+
+export default class SearchBar extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    this.props.onChange(event);
+    this.setState({ value: event.target.value });
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className={'searchbar rounded flex flex-row'}>
+        <span className={'flex items-center px-3'}>
+          <svg className={'h-6 w-6'} fill={'none'} viewBox={'0 0 24 24'} stroke={'#fff'}>
+            <path
+              strokeLinecap={'round'}
+              strokeLinejoin={'round'}
+              strokeWidth={2}
+              d={'M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'}
+            />
+          </svg>
+        </span>
+
+        <input
+          className={'flex-1 pl-1 pr-4 rounded-r bg-transparent'}
+          spellCheck={false}
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
--- src/pages/panel.tsx
+++ src/pages/panel.tsx
@@ -2,11 +2,39 @@ import React from 'react';
 
 import '@styles/panel.sass';
 import SearchEntry from '@components/search-entry';
+import SearchBar from '@components/searchbar';
 
 export default class Panel extends React.Component {
   public render(): JSX.Element {
     return (
-      <div className={'panel flex justify-center'}>
+      <div className={'panel'}>
+        <SearchBar onChange={(e) => console.log(e.target.value)} />
+        <SearchEntry
+          songdata={{
+            title: "(I've Had) The Time Of My Life",
+            artist: 'Bill Medley and Jennifer Warnes',
+            source: '',
+            unlimited: false,
+            img_url: 'https://static.wikia.nocookie.net/justdance/images/4/44/Thetimeofmylife_jd4_cover_generic.png',
+            difficulty: 1,
+            coaches: 1,
+            effort: 1,
+          }}
+        />
+
+        <SearchEntry
+          songdata={{
+            title: "(I've Had) The Time Of My Life",
+            artist: 'Bill Medley and Jennifer Warnes',
+            source: '',
+            unlimited: false,
+            img_url: 'https://static.wikia.nocookie.net/justdance/images/4/44/Thetimeofmylife_jd4_cover_generic.png',
+            difficulty: 1,
+            coaches: 1,
+            effort: 1,
+          }}
+        />
+
         <SearchEntry
           songdata={{
             title: "(I've Had) The Time Of My Life",
