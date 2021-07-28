27b0c1948d165f4430eb4c13efdbfe0f93b34843
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-07-28 20:27:41 +0200

    style(#1): Split and Extract Sass files

--- src/components/search-entry.tsx
+++ src/components/search-entry.tsx
@@ -10,11 +10,11 @@ interface Props {
 export default class SearchEntry extends React.Component<Props> {
   public render(): JSX.Element {
     return (
-      <div className={'select-none flex flex-row flex-nowrap search-entry search-entry-background rounded'}>
+      <div className={'search-entry search-entry-background select-none flex flex-row flex-nowrap rounded'}>
         <img className={'flex-shrink flex-20 rounded-l min-w-0'} draggable={false} src={this.props.songdata.img_url} />
         <div className={'flex-grow-4 flex-shrink-0 flex-80 p-2 min-w-0 self-center'}>
-          <p className={'search-entry-text search-entry-title truncate'}>{this.props.songdata.title}</p>
-          <p className={'search-entry-text search-entry-artist truncate'}>{this.props.songdata.artist}</p>
+          <p className={'search-entry-title truncate'}>{this.props.songdata.title}</p>
+          <p className={'search-entry-artist truncate'}>{this.props.songdata.artist}</p>
         </div>
       </div>
     );
