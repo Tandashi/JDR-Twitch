25afefca2328a6eb0f469c5eecf141c140b11bd3
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-08-20 01:15:45 +0200

    style: Make overlay using translation and animation not via width

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -14,10 +14,10 @@ interface Props {
 
 export default class Overlay extends React.Component<Props> {
   public render(): JSX.Element {
-    const width = this.props.isOpen ? 'w-full' : 'w-0';
+    const slideClass = this.props.isOpen ? 'slide-out' : 'slide-in';
     const fixturePoint = this.props.direction === OverlayDirection.LEFT ? 'left-0' : 'right-0';
     return (
-      <div className={`overlay h-full fixed z-10 overflow-x-hidden top-0 ${fixturePoint} ${width}`}>
+      <div className={`overlay h-full w-full ${slideClass} fixed top-0 ${fixturePoint} z-10 overflow-x-hidden`}>
         {this.props.children}
       </div>
     );
