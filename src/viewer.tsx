6810478dcc8f88ec032f82cd4c7e6e70c758f8f4
Tandashi <18377875+Tandashi@users.noreply.github.com>
2021-08-20 19:20:54 +0200

    fix(#22): Check if user permission was granted

--- src/viewer.tsx
+++ src/viewer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Panel from '@pages/panel';
+import UnauthorizedPage from '@pages/unauthorized';
 
 import ConfigService from '@services/config-service';
 
@@ -14,5 +15,16 @@ window.Twitch.ext.onAuthorized((auth) => {
     },
   });
 
-  ReactDOM.render(<Panel />, document.getElementById('root'));
+  const parts = auth.token.split('.');
+  const payload = JSON.parse(window.atob(parts[1]));
+
+  // Check if user has granted permission
+  if (payload.user_id) {
+    // user HAS granted
+    ReactDOM.render(<Panel />, document.getElementById('root'));
+  }
+  else {
+    // user has NOT granted
+    ReactDOM.render(<UnauthorizedPage />, document.getElementById('root'));
+  }
 });
