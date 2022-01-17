e818d1637aef66b13efcac17c4df2a53d7d862e1
Tandashi <18377875+Tandashi@users.noreply.github.com>
2022-01-17 13:04:34 +0100

    feat: Display Twitter and Statuspage while loading

--- src/components/form/loading-indicator.tsx
+++ src/components/form/loading-indicator.tsx
@@ -6,11 +6,22 @@ export default class LoadingIndicator extends React.Component {
   public render(): JSX.Element {
     return (
       <div className='flex items-center justify-center flex-1'>
-        <div className='flex flex-1  flex-col justify-center items-center space-x-1 text-white'>
+        <div className='flex flex-1 flex-col justify-center items-center space-x-1 text-white'>
           <div className={'w-24 h-24 animate-pulse'}>
             <SushiRollsTableIcon />
           </div>
           <p className={'text-xs md:text-base font-bold'}>Loading ...</p>
+          <p className={'text-xs md:text-base mt-10'}>
+            If loading takes a long time please check the{' '}
+            <a target='_blank' className={'font-medium text-blue-300'} href={'https://status.tandashi.de/status'}>
+              Status Page
+            </a>{' '}
+            or{' '}
+            <a target='_blank' className={'font-medium text-blue-300'} href={'https://twitter.com/JDR_Twitch'}>
+              Twitter
+            </a>
+            .
+          </p>
         </div>
       </div>
     );
